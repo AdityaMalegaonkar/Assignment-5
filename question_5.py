078b49563e5fb6b4fdcbb4b77e5d70a2147000c3
Aditya Malegaonkar <112890202+AdityaMalegaonkar@users.noreply.github.com>
2022-10-14 20:40:01 +0530

    Update question_5.py

--- question_5.py
+++ question_5.py
@@ -4,8 +4,8 @@
 
 class account:
     def __init__(self , title = None , balance = 0):
-        self.title = input("Enter your name :")
-        self.balance = int(input("Enter your Account Balance :"))
+        self.title = title
+        self.balance = balance
 
     def withdrawal(self , amount):
         self.amount = amount
@@ -24,21 +24,24 @@ class account:
 class savingsaccount(account):
     def __init__(self , title = None , balance = 0 , interestrate = 0):
         super().__init__(title , balance)
-        self.interestrate = int(input("Enter interestrate :"))
+        self.interestrate = interestrate
 
     def interestamount(self):
         interest_amount = self.interestrate * self.balance / 100
-        print("The Interest amount is :", interest_amount)
+        print("The Interest amount is :" , interest_amount)
     
     def display(self):
         print("(" , self.title , "," , self.balance , "," , self.interestrate , ")")
 
-account_obj = account()
+title = input("Enter your name :")
+balance = int(input("Enter your account balance :"))
+account_obj = account(title , balance)
 account_obj.withdrawal(int(input("Enter amount you want to withdraw :")))
 account_obj.get_balance()
 account_obj.deposit(int(input("Enter amount you want to deposit :")))
 account_obj.get_balance()
 account_obj.display()
-savingsaccount_obj = savingsaccount()
+interestrate = int(input("Enter interestrate :"))
+savingsaccount_obj = savingsaccount(title , balance , interestrate)
 savingsaccount_obj.interestamount()
 savingsaccount_obj.display()
